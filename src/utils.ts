463fdb8f450cbbdc223049136599c7b4a548023f
Tomáš Heky Hekrla <tomas@heky.eu>
2021-07-11 17:01:35 +0200

    refactor: shuffle func

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,5 @@
  * Shuffle array and return new instance.
  */
 export function shuffle<T>(array: T[]): T[] {
-  const newArray = [...array];
-  newArray.sort(() => (Math.random() > 0.5 ? 1 : -1));
-  return newArray;
+  return [...array].sort(() => (Math.random() > 0.5 ? 1 : -1));
 }
