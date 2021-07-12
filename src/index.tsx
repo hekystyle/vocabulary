8211b21aacc22cd71b00776639bd4fb87e3a0703
Tomáš Hekrla <hekrla@enmon.tech>
2021-07-12 20:27:42 +0200

    add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
