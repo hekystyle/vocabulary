859ccedca5d412423e4ac26a68e9a6e155f5325b
Tomáš Hekrla (Heky) <tomas@heky.eu>
2021-09-04 21:53:42 +0200

    refactor(utils): delete prepareAnswersSet func

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,20 +1,6 @@
-import { shuffle } from "./shuffle";
-
 export interface Answer<T> {
   isCorrect: boolean;
   entity: T;
-}
-
-export function prepareAnswersSet<T>(tested: T, all: T[]): Answer<T>[] {
-  const allWithoutTested = all.filter((p) => p !== tested);
-  const shufledAllWithoutTested = shuffle(allWithoutTested);
-  const incorrects = shufledAllWithoutTested.splice(0, 3);
-  const set = [
-    { isCorrect: true, entity: tested },
-    ...incorrects.map((p) => ({ isCorrect: false, entity: p })),
-  ];
-  const shufledSet = shuffle(set);
-  return shufledSet;
 }
 
 export function sortImmutable<T>(
