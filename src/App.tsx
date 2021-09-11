6fdb87708838cc76b1ecbc1931746daace2a463f
Tomáš Hekrla (Heky) <tomas@heky.eu>
2021-09-11 16:30:28 +0200

    feat: scrollable layout content

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,24 @@ const StyledApp = styled.div`
   font-size: calc(10px + 2vmin);
 `;
 
+const StyledContent = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
 export function App() {
   return (
     <HashRouter>
       <StyledApp className="bg-dark">
         <NavBar />
-        <Switch>
-          <Route path="/record" component={RecordRouter} />
-          <Route path="/practice" component={PracticeRouter} />
-          <Route path="/list" component={ListRouter} />
-          <Redirect to="/list" />
-        </Switch>
+        <StyledContent>
+          <Switch>
+            <Route path="/record" component={RecordRouter} />
+            <Route path="/practice" component={PracticeRouter} />
+            <Route path="/list" component={ListRouter} />
+            <Redirect to="/list" />
+          </Switch>
+        </StyledContent>
       </StyledApp>
     </HashRouter>
   );
