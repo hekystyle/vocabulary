8805550baa816c637e05920d883a49d037e06af2
Tomáš Hekrla <hekrla@enmon.tech>
2021-09-15 09:57:54 +0200

    feat: new shuffle algo

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,6 +1,21 @@
 /**
  * Shuffle array and return new instance.
  */
-export function shuffle<T>(array: T[]): T[] {
-  return [...array].sort(() => (Math.random() > 0.5 ? 1 : -1));
+export function shuffle<T>(source: readonly T[]): T[] {
+  const copy = [...source];
+
+  let total = copy.length;
+
+  // While there remain elements to shuffle…
+  while (total) {
+    // Pick a remaining element…
+    const index = Math.floor(Math.random() * total--);
+
+    // And swap it with the current element.
+    const temp = copy[total];
+    copy[total] = copy[index];
+    copy[index] = temp;
+  }
+
+  return copy;
 }
