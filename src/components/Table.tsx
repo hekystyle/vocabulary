99561fce76aef6eff6e63e58a5cd368f0c724d8e
Tomáš Hekrla (Heky) <tomas@heky.eu>
2021-09-18 14:59:18 +0200

    fix: console error in Table component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 import { Table as AntdTable, TableProps } from "antd";
 
-export function Table<T extends {}>(props: TableProps<T>) {
-  const Component = styled((props: TableProps<T>) => (
-    <AntdTable<T> {...props} />
-  ))`
-    .ant-pagination {
-      background-color: white;
-      padding: 16px 0;
-      margin: 0;
-    }
-  `;
+const StyledTable = styled(AntdTable)`
+  .ant-pagination {
+    background-color: white;
+    padding: 16px 0;
+    margin: 0;
+  }
+`;
 
-  return <Component {...props} />;
+export function Table<T>(props: TableProps<T>) {
+  // @ts-expect-error
+  return <StyledTable {...props} />;
 }
