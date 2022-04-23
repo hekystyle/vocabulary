c26b0687a18949f28f8abe49485de4bc051c0c65
Tomáš Hekrla (Heky) <tomas@heky.eu>
2022-04-24 00:13:20 +0200

    refactor: data migration (#98)
    * refactor: move component
    
    * refactor: data migration

---
+++ src/containers/DataMigration/migrations/20220319144900.ts
@@ -0,0 +1,30 @@
+import { db } from 'db';
+import { isArrayOf } from 'utils/isArrayOf';
+import { TermV1 } from 'types/Term';
+import { isTermV1 } from 'types/isTerm';
+import type { Migration } from '../DataMigration';
+
+const VOCABULARY_KEY = 'vocabulary';
+
+function loadState(): TermV1[] | undefined {
+  const json = localStorage.getItem(VOCABULARY_KEY);
+
+  if (json === null) return undefined;
+
+  const value: unknown = JSON.parse(json);
+
+  return isArrayOf(value, isTermV1) ? value : undefined;
+}
+
+export const up: Migration = async () => {
+  const terms = loadState();
+  if (terms === undefined) return;
+  for (const term of terms) {
+    await db.terms.add({
+      ...term,
+      id: undefined,
+      createdAt: new Date(term.id),
+    });
+  }
+  localStorage.removeItem(VOCABULARY_KEY);
+};
