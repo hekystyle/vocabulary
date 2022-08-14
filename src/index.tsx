de405febb9148faef57273337f1fb6243296c122
Tomáš Hekrla (Heky) <tomas@heky.eu>
2022-08-14 12:01:41 +0200

    feat: validate dictionary API result by class validator (#189)
    * feat: validate dictionary API result by class validator
    
    * chore(lint): allow devDeps for tests setup file

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'antd/dist/antd.css';
--- src/services/dictionaryApi.ts
+++ src/services/dictionaryApi.ts
@@ -1,27 +1,63 @@
-export interface Word {
-  meanings: Meaning[];
+/* eslint-disable max-classes-per-file */
+import { plainToInstance, Type } from 'class-transformer';
+import { IsArray, IsDefined, IsString, ValidateNested, validateOrReject } from 'class-validator';
+
+export interface IWord {
+  meanings: IMeaning[];
 }
 
-export interface Meaning {
+export interface IMeaning {
   partOfSpeech: string;
-  definitions: Definition[];
+  definitions: IDefinition[];
 }
 
-export interface Definition {
+export interface IDefinition {
   definition: string;
+}
+
+export class Definition implements IDefinition {
+  @IsDefined()
+  @IsString()
+  definition = '';
+}
+
+export class Meaning implements IMeaning {
+  @IsDefined()
+  @IsString()
+  partOfSpeech = '';
+
+  @IsDefined()
+  @IsArray()
+  @Type(() => Definition)
+  @ValidateNested({ each: true })
+  definitions: Definition[] = [];
+}
+
+export class Word implements IWord {
+  @IsDefined()
+  @IsArray()
+  @Type(() => Meaning)
+  @ValidateNested({ each: true })
+  meanings: Meaning[] = [];
 }
 
 export class DictionaryApi {
   public static async word(word: string, { signal }: Pick<RequestInit, 'signal'> = {}): Promise<Word[]> {
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`, {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${encodeURIComponent(word)}`, {
       headers: {
         Accept: 'application/json',
       },
       signal,
     });
 
-    // TODO: add validation (https://github.com/hekystyle/vocabulary/issues/14)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return await response.json();
+    const result: unknown = await response.json();
+
+    if (!Array.isArray(result)) throw new Error(`Invalid response, expected array, got ${typeof result}`);
+
+    const instances = plainToInstance(Word, result);
+
+    await Promise.all(instances.map(async i => validateOrReject(i)));
+
+    return instances;
   }
 }
