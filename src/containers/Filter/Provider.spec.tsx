35770d86cf1ffeeea8901df10706bed848686b04
Tomáš Hekrla <tomas@heky.eu>
2022-09-18 13:36:21 +0200

    test: update name

--- src/containers/Filter/Provider.spec.tsx
+++ src/containers/Filter/Provider.spec.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { HashRouter } from 'react-router-dom';
 import { FilterProvider } from './Provider';
 
-it('renders', () => {
+it('should render without error', () => {
   expect(() =>
     render(
       <HashRouter>
