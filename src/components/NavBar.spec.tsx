2bd23e53edf3720aedffb73ec02b6d12d65734b1
Tomáš Hekrla <tomas@heky.eu>
2022-09-21 00:11:57 +0200

    test: click on link in NavBar

--- src/components/NavBar.spec.tsx
+++ src/components/NavBar.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { HashRouter } from 'react-router-dom';
 import { NavBar } from './NavBar';
 
@@ -10,4 +11,18 @@ test('renders without error', () => {
       </HashRouter>,
     ),
   ).not.toThrow();
+});
+
+it('should navigate to the list page', async () => {
+  const user = userEvent.setup();
+
+  render(
+    <HashRouter>
+      <NavBar />
+    </HashRouter>,
+  );
+
+  await user.click(screen.getByRole('button', { name: 'List' }));
+
+  expect(window.location.hash).toBe('#/list');
 });
