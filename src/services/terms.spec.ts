44d218c1e4f03694d7374173c38ed51ea6cd673f
Tomáš Hekrla <tomas@heky.eu>
2022-09-21 21:22:19 +0200

    test: use before & after each

--- src/services/terms.spec.ts
+++ src/services/terms.spec.ts
@@ -3,21 +3,30 @@ import { AppDb } from 'db';
 import fakeIndexedDB from 'fake-indexeddb';
 import FDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange';
 import { Term } from 'types/Term';
-import { IndexedDbTermsRepository } from './terms';
+import { IndexedDbTermsRepository, ITermsRepository } from './terms';
 
 AppDb.dependencies.indexedDB = fakeIndexedDB;
 AppDb.dependencies.IDBKeyRange = FDBKeyRange;
 
+let db: AppDb;
+let repository: ITermsRepository;
+
+beforeEach(() => {
+  db = new AppDb(randomUUID());
+  repository = new IndexedDbTermsRepository(db);
+});
+
+afterEach(async () => {
+  await db.delete();
+  db.close();
+});
+
 it('should create instance', async () => {
-  const db = new AppDb(randomUUID());
   expect(new IndexedDbTermsRepository(db)).toBeInstanceOf(IndexedDbTermsRepository);
   await db.delete();
 });
 
 it('should get terms', async () => {
-  const db = new AppDb(randomUUID());
-  const repository = new IndexedDbTermsRepository(db);
-
   await db.terms.bulkAdd([{} as Term, {} as Term]);
 
   const result = await repository.get({ page: 1, pageSize: 1 });
@@ -31,27 +40,17 @@ it('should get terms', async () => {
     },
   ]);
   expect(result.total).toBe(2);
-
-  await db.delete();
 });
 
 it('should get term by id', async () => {
-  const db = new AppDb(randomUUID());
-  const repository = new IndexedDbTermsRepository(db);
-
   const id = await db.terms.add({} as Term);
 
   const result = await repository.getById(id);
 
   expect(result).toBeDefined();
-
-  await db.delete();
 });
 
 it('should create term', async () => {
-  const db = new AppDb(randomUUID());
-  const repository = new IndexedDbTermsRepository(db);
-
   const result = await repository.create({} as Term);
 
   expect(result).toEqual({
@@ -60,54 +59,34 @@ it('should create term', async () => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     createdAt: expect.any(Date),
   });
-
-  await db.delete();
 });
 
 it('should update term', async () => {
-  const db = new AppDb(randomUUID());
-  const repository = new IndexedDbTermsRepository(db);
-
   const id = await db.terms.add({ word: 'word1' } as Term);
 
   const result = await repository.update({ id, word: 'word2' } as Term);
 
   expect(result).toEqual({ id, word: 'word2' });
-
-  await db.delete();
 });
 
 it('should delete term', async () => {
-  const db = new AppDb(randomUUID());
-  const repository = new IndexedDbTermsRepository(db);
-
   const id = await db.terms.add({} as Term);
 
   await repository.delete(id);
 
   const result = await db.terms.get(id);
   expect(result).toBeUndefined();
-
-  await db.delete();
 });
 
 it('should get terms words', async () => {
-  const db = new AppDb(randomUUID());
-  const repository = new IndexedDbTermsRepository(db);
-
   await db.terms.bulkAdd([{ word: 'foo1' } as Term, { word: 'foo2' } as Term, { word: 'bar' } as Term]);
 
   const result = await repository.getWords('foo');
 
   expect(result).toEqual(['foo1', 'foo2']);
-
-  await db.delete();
 });
 
 it('should get unique parts of speech', async () => {
-  const db = new AppDb(randomUUID());
-  const repository = new IndexedDbTermsRepository(db);
-
   await db.terms.bulkAdd([
     { partOfSpeech: 'foo' } as Term,
     { partOfSpeech: 'foo' } as Term,
@@ -117,6 +96,4 @@ it('should get unique parts of speech', async () => {
 
   const result = await repository.getUniquePartsOfSpeech();
   expect(result).toEqual(['foo', 'bar']);
-
-  await db.delete();
 });
