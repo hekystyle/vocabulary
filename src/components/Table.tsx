98b57fb0c3d36e5fa4815afa86059f8f08510106
Tomáš Hekrla <tomas@heky.eu>
2022-09-24 20:05:14 +0200

    feat: dark antd theme

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,2 @@
-import { FC } from 'react';
-import styled from 'styled-components';
-import { Table as AntdTable, TableProps } from 'antd';
-
-const StyledTable = styled(AntdTable)`
-  .ant-pagination {
-    background-color: white;
-    padding: 16px 0;
-    margin: 0;
-  }
-`;
-
-// eslint-disable-next-line react/function-component-definition
-export function Table<T>(props: TableProps<T>): JSX.Element {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <StyledTable<FC<TableProps<T>>> {...props} />;
-}
+export { Table } from 'antd';
+export type { TableProps } from 'antd';
