dae5ff4def79ed09171d999980e78995607451be
Tomáš Hekrla <tomas@heky.eu>
2022-09-25 11:50:42 +0200

    test: fix

--- src/services/terms.spec.ts
+++ src/services/terms.spec.ts
@@ -27,7 +27,7 @@ it('should create instance', async () => {
 });
 
 it('should get terms', async () => {
-  await db.terms.bulkAdd([{} as Term, {} as Term]);
+  await db.terms.bulkAdd([{ word: 'a' } as Term, { word: 'b' } as Term]);
 
   const result = await repository.get({ page: 1, pageSize: 1, sortField: 'word', sortOrder: 'ascend' });
 
@@ -35,6 +35,8 @@ it('should get terms', async () => {
     {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       id: expect.any(Number),
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      word: expect.stringMatching(/a|b/),
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       createdAt: expect.any(Date),
     },
