0c27ad8d8d912010c28b8bd432e4b51dab269f03
Tomáš Hekrla <tomas@heky.eu>
2022-10-02 17:13:59 +0200

    test: Spinner.role

--- src/components/SpinnerBox.spec.tsx
+++ src/components/SpinnerBox.spec.tsx
@@ -1,6 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { SpinnerBox } from './SpinnerBox';
 
 test('renders without error', () => {
   expect(() => render(<SpinnerBox />)).not.toThrow();
+});
+
+it('should has role attribute', () => {
+  render(<SpinnerBox />);
+  expect(screen.getByRole('status')).toBeInTheDocument();
 });
