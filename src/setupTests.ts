989830b5e089be7b70b42ab873a5586e8bda7606
Tomáš Hekrla <tomas@heky.eu>
2022-10-02 19:55:11 +0200

    test: write for App

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,3 +5,29 @@
 import '@testing-library/jest-dom';
 import 'reflect-metadata';
 import 'fake-indexeddb/auto';
+
+window.matchMedia = query => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(), // deprecated
+  removeListener: jest.fn(), // deprecated
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
+
+window.speechSynthesis = {
+  onvoiceschanged: jest.fn(),
+  paused: false,
+  pending: false,
+  speaking: false,
+  cancel: jest.fn(),
+  getVoices: jest.fn(),
+  pause: jest.fn(),
+  resume: jest.fn(),
+  speak: jest.fn(),
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+};
