475f56075a1d7f7f677f7e7f9bc6c2788ecbaae7
Tomáš Hekrla <tomas@heky.eu>
2022-10-02 17:13:49 +0200

    feat: set Spinner.role

--- src/components/SpinnerBox.tsx
+++ src/components/SpinnerBox.tsx
@@ -3,7 +3,7 @@ import { Spinner } from 'react-bootstrap';
 
 export const SpinnerBox: FC<{ children?: React.ReactNode }> = ({ children }) => (
   <div className="d-flex flex-column align-items-center gap-3">
-    <Spinner animation="border" />
+    <Spinner animation="border" role="status" />
     {children}
   </div>
 );
